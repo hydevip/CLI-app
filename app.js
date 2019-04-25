ec3a5afb4907e94f826af79b78bd3af75687f3f4
hydevip <35841222+hydevip@users.noreply.github.com>
2019-04-25 17:07:16 +0200

    unused console.log lines removed

--- app.js
+++ app.js
@@ -4,49 +4,49 @@ const program = require('commander');
 const { getToken } = require('./lib/config');
 
 const { registerUser, loginUser } = require('./lib/user');
-const { addCustomer, listCustomer, searchCustomer } =require('./lib/customer');
+const { addCustomer, listCustomer, searchCustomer } = require('./lib/customer');
 
 
 
 program
   .version('0.1.0');
 
-  token=getToken();
+token = getToken();
 //console.log(token);
-if (!token) {
-  program
-    .command('register <username> <password>')
-    .alias('r')
-    .description('Register a new user.')
-    .action((username, password) => registerUser(username, password));
 
-  program
-    .command('login <username> <password>')
-    .alias('l')
-    .description('Login in order to manage customers.')
-    .action((username, password) => loginUser(username, password));
-} else {
+program
+  .command('register <username> <password>')
+  .alias('r')
+  .description('Register a new user.')
+  .action((username, password) => registerUser(username, password));
 
-  program
-    .command('new <name> <email> <phone>')
-    .alias('n')
-    .description('Create a new customer.')
-    .action((name,email,phone)=> addCustomer(name,email,phone));
+program
+  .command('login <username> <password>')
+  .alias('l')
+  .description('Login in order to manage customers.')
+  .action((username, password) => loginUser(username, password));
 
-  
-    
+
+if (token) {
+  program
+  .command('new <name> <email> <phone>')
+  .alias('n')
+  .description('Create a new customer.')
+  .action((name, email, phone) => addCustomer(name, email, phone));
+
+
+
   program
     .command('list')
     .alias('l')
     .description('List all customers.')
-    .action(()=>listCustomer());
+    .action(() => listCustomer());
 
   program
     .command('search <name>')
     .alias('s')
     .description('Search a  customer.')
-    .action((name)=>searchCustomer(name));
-
+    .action((name) => searchCustomer(name));
 }
 
 program.parse(process.argv);
